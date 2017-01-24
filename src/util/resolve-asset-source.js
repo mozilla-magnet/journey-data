a295d1fd9648024ecc43e1fdd93cae77bcfb4b61
Sam Giles <sam.e.giles@gmail.com>
2017-01-24 17:29:02 +0000

    Resolve static..

--- src/util/resolve-asset-source.js
+++ src/util/resolve-asset-source.js
@@ -1,11 +1,7 @@
 let resolveAsset;
 try {
   const resolveAssetSource =  require('react-native/Libraries/Image/resolveAssetSource');
-
-  resolveAsset = function(path) {
-    return resolveAssetSource(require(path));
-  };
-
+  resolveAsset = resolveAssetSource;
 } catch(e) {
   resolveAsset = function(path) {
     return { uri: path };
